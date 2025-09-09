b3f5349fb83a3c239e666d5719fbf201bab781ba
Laura Berge <lauraeberge@gmail.com>
2025-09-09 14:38:36 -0500

    move lab to GitHub

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// Utility Functions
+
+/**
+ * Capitalizes the first letter of each word in the input string.
+ * @param {string} input - The input string.
+ * @returns {string} - The formatted string.
+ */
+function capitalizeWords(input) {
+    return input.replace(/\b\w/g, char => char.toUpperCase());
+}
+
+/**
+ * Filters active users from the array.
+ * @param {Array} users - An array of user objects.
+ * @returns {Array} - An array of active user objects.
+ */
+function filterActiveUsers(users) {
+    return users.filter(user => user.isActive);
+}
+
+/**
+ * Logs an action performed by a user with a timestamp.
+ * @param {string} action - The action performed.
+ * @param {string} username - The name of the user.
+ * @returns {string} - The log message.
+ */
+function logAction(action, username) {
+    const timestamp = new Date().toISOString();
+    return `User ${username} performed ${action} at ${timestamp}`;
+}
+
+module.exports = { capitalizeWords, filterActiveUsers, logAction };
